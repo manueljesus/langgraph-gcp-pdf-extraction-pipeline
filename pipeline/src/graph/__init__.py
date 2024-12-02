4c944d5aacdab2ccfe5aa5507998f7860aa1d696
Manuel Jesús Escamilla <manueljesus@mjescamilla.com>
2024-12-02 13:02:02 +0100

    feat(pipeline): Get File LangGraph-compatible node.
    Node to retrieve a file from GCS bucket via its corresponding task.
    It sends the loaded data and file unique hash to the shared state.

--- pipeline/src/graph/__init__.py
+++ pipeline/src/graph/__init__.py
@@ -1,5 +1,6 @@
 from .pipeline_state import PipelineState
 from .graph_error import GraphError
+from .get_file_node import GetFile
 from .load_pdf_node import LoadPDF
 from .extract_metadata_node import ExtractMetadata
 from .extract_summary_and_keywords_node import ExtractSummaryAndKeywords
---
+++ pipeline/test/unit/graph/test_get_file_node.py
@@ -0,0 +1,46 @@
+import pytest
+from unittest.mock import patch, MagicMock
+from typing import Generator
+from io import BytesIO
+from src.graph import GetFile
+
+
+class TestGetFileNode:
+    @pytest.fixture()
+    def mock_file(self) -> BytesIO:
+        return BytesIO(b"Mocked file content")
+
+    @pytest.fixture()
+    def mock_get_file_from_bucket(
+        self,
+        mock_file: BytesIO
+    ) -> Generator[MagicMock, None, None]:
+        with patch("src.graph.get_file_node.get_file_from_bucket", return_value=mock_file) as mock:
+            yield mock
+
+    @pytest.fixture()
+    def mock_generate_file_hash(self) -> Generator[MagicMock, None, None]:
+        with patch("src.graph.get_file_node.generate_file_hash", return_value="mocked_paper_id") as mock:
+            yield mock
+
+    def test_get_file_state(
+        self,
+        mock_file: BytesIO,
+        mock_get_file_from_bucket: MagicMock,
+        mock_generate_file_hash: MagicMock,
+    ) -> None:
+        """Test GetFile to verify the state output."""
+        file_name = "dummy_file.pdf"
+        get_file_node = GetFile(file_name)
+
+        result = get_file_node({"state": {}})
+
+        mock_get_file_from_bucket.assert_called_once_with(file_name)
+        mock_generate_file_hash.assert_called_once_with(mock_file)
+
+        assert result == {
+            "state": {
+                "file": mock_file,
+                "paper_id": "mocked_paper_id"
+            }
+        }
