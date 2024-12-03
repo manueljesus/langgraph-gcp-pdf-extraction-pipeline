25ccfce8487de9699b698a17b69ead6fa6416fe4
Manuel Jesús Escamilla <manueljesus@mjescamilla.com>
2024-12-03 21:49:06 +0100

    feat(pipeline): Add logging into the Extract Metadata LangGraph node

--- pipeline/test/unit/graph/test_extract_metadata_node.py
+++ pipeline/test/unit/graph/test_extract_metadata_node.py
@@ -14,15 +14,25 @@ class TestExtractMetadataNode:
             yield mock
 
     @pytest.fixture()
+    def mock_logger(self) -> Generator[MagicMock, None, None]:
+        with patch("src.graph.extract_metadata_node.logger") as mock_logger:
+            yield mock_logger
+
+    @pytest.fixture()
     def mock_pipeline_state(self) -> PipelineState:
         return {
-            "state": {"text": "Mocked extracted text"}
+            "state": {
+                "text": "Mocked extracted text",
+                "paper_id": "paper_id"
+            }
         }
 
     @pytest.fixture()
     def mock_pipeline_state_with_error(self) -> PipelineState:
         return {
-            "state": {}  # No "text" key to trigger an error
+            "state": {
+                "paper_id": "paper_id"  # Missing "text" key to trigger an error
+            }
         }
 
     @pytest.fixture()
@@ -33,12 +43,14 @@ class TestExtractMetadataNode:
         self,
         mock_pipeline_state: PipelineState,
         mock_extract_metadata_task: MagicMock,
+        mock_logger: MagicMock,
         extract_metadata: ExtractMetadata
     ) -> None:
         """Test ExtractMetadata node to verify the state output."""
         result = extract_metadata(mock_pipeline_state)
 
         mock_extract_metadata_task.assert_called_once_with("Mocked extracted text")
+        mock_logger.info.assert_called_once_with("Extracting metadata from paper ID paper_id")
         assert result == {
             "state": {
                 "metadata": "metadata"
@@ -48,8 +60,12 @@ class TestExtractMetadataNode:
     def test_extract_metadata_raises_graph_error(
         self,
         mock_pipeline_state_with_error: PipelineState,
+        mock_logger: MagicMock,
         extract_metadata: ExtractMetadata
     ) -> None:
         """Test ExtractMetadata raises GraphError on exception."""
         with pytest.raises(GraphError):
             extract_metadata(mock_pipeline_state_with_error)
+        mock_logger.error.assert_called_once_with(
+            "Failed to extract metadata from paper ID paper_id: 'text'"
+        )
