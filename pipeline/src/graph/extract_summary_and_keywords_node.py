928231a3852014dc5642c287081bb84bc33d9d9f
Manuel Jesús Escamilla <manueljesus@mjescamilla.com>
2024-12-03 21:55:34 +0100

    feat(pipeline): Add logging into the Extract Summary and Keywords LangGraph node

--- pipeline/src/graph/extract_summary_and_keywords_node.py
+++ pipeline/src/graph/extract_summary_and_keywords_node.py
@@ -1,11 +1,16 @@
 from src.graph import PipelineState, GraphError
 from typing import Any
 from src.tasks import extract_summary_and_keywords
+from src.logger import get_logger
+
+logger = get_logger(__name__)
 
 
 class ExtractSummaryAndKeywords:
     def __call__(self, state: PipelineState) -> Any:
         try:
+            logger.info(f"Extracting summary and keywords from paper ID {state.get('state', {}).get('paper_id', None)}")
             return {"state": {"summary": extract_summary_and_keywords(state["state"]["text"])}}
         except Exception as e:
+            logger.error(f"Failed to extract summary and keywords from paper ID {state.get('state', {}).get('paper_id', None)}: {e}")
             raise GraphError(e)
