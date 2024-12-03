29656834e2c22f6a9f20ee7b11457dd09bbc797b
Manuel Jesús Escamilla <manueljesus@mjescamilla.com>
2024-12-03 17:37:14 +0100

    feat(pipeline): Add logging into extract_key_research_findings_and_methodology task

--- pipeline/test/unit/tasks/test_extract_key_research_findings_and_methodology.py
+++ pipeline/test/unit/tasks/test_extract_key_research_findings_and_methodology.py
@@ -1,12 +1,21 @@
+import pytest
+from typing import Generator
 from unittest.mock import MagicMock, patch
 from src.tasks.extract_key_research_findings_and_methodology import extract_key_research_findings_and_methodology
 
 
 class TestExtractMetadata:
+    @pytest.fixture
+    def mock_logger(self) -> Generator[MagicMock, None, None]:
+        """Fixture to patch the logger."""
+        with patch("src.tasks.extract_key_research_findings_and_methodology.logger") as mock_logger:
+            yield mock_logger
+
     @patch("src.tasks.extract_key_research_findings_and_methodology.vertex_ai_llama_request")
     def test_extract_key_research_findings_success(
         self,
-        mock_llm_request: MagicMock
+        mock_llm_request: MagicMock,
+        mock_logger: MagicMock
     ):
         mock_llm_request.return_value = """{
             "methodology": "We conducted a series of experiments using a randomized controlled trial design to ...",
@@ -21,11 +30,13 @@ class TestExtractMetadata:
         }
 
         assert extract_key_research_findings_and_methodology(input_text) == expected_output
+        mock_logger.info.assert_called_once_with("Extracting key research findings and methodology")
 
     @patch("src.tasks.extract_key_research_findings_and_methodology.vertex_ai_llama_request")
     def test_extract_key_research_findings_partial_data(
         self,
-        mock_llm_request: MagicMock
+        mock_llm_request: MagicMock,
+        mock_logger: MagicMock
     ):
         mock_llm_request.return_value = """{
             "methodology": "We conducted a series of experiments using a randomized controlled trial design to ...",
@@ -40,11 +51,13 @@ class TestExtractMetadata:
         }
 
         assert extract_key_research_findings_and_methodology(input_text) == expected_output
+        mock_logger.info.assert_called_once_with("Extracting key research findings and methodology")
 
     @patch("src.tasks.extract_key_research_findings_and_methodology.vertex_ai_llama_request")
     def test_extract_key_research_findings_error_handling(
         self,
-        mock_llm_request: MagicMock
+        mock_llm_request: MagicMock,
+        mock_logger: MagicMock
     ):
         mock_llm_request.side_effect = Exception("Mocked exception")
 
@@ -56,3 +69,4 @@ class TestExtractMetadata:
         }
 
         assert extract_key_research_findings_and_methodology(input_text) == expected_output
+        mock_logger.error.assert_called_once_with("Error extracting key research findings and methodology: Mocked exception")
