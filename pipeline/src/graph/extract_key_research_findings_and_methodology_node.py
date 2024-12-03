a26ed7f19dfd872fd0cb8c8620d65f737a84e68e
Manuel Jesús Escamilla <manueljesus@mjescamilla.com>
2024-12-03 21:53:26 +0100

    feat(pipeline): Add logging into the Extract Metadata LangGraph node

--- pipeline/src/graph/extract_key_research_findings_and_methodology_node.py
+++ pipeline/src/graph/extract_key_research_findings_and_methodology_node.py
@@ -1,11 +1,16 @@
 from src.graph import PipelineState, GraphError
 from typing import Any
 from src.tasks.extract_key_research_findings_and_methodology import extract_key_research_findings_and_methodology
+from src.logger import get_logger
+
+logger = get_logger(__name__)
 
 
 class ExtractKeyResearchFindingsAndMethodology:
     def __call__(self, state: PipelineState) -> Any:
         try:
+            logger.info(f"Extracting key research findings and methodology from paper ID {state.get('state', {}).get('paper_id', None)}")
             return {"state": {"research": extract_key_research_findings_and_methodology(state["state"]["text"])}}
         except Exception as e:
+            logger.error(f"Failed to extract key research findings and methodology from paper ID {state.get('state', {}).get('paper_id', None)}: {e}")
             raise GraphError(e)
--- pipeline/test/unit/graph/test_extract_key_research_findings_and_methodology_node.py
+++ pipeline/test/unit/graph/test_extract_key_research_findings_and_methodology_node.py
@@ -14,15 +14,25 @@ class TestExtractKeyResearchFindingsAndMethodologyNode:
             yield mock
 
     @pytest.fixture()
+    def mock_logger(self) -> Generator[MagicMock, None, None]:
+        with patch("src.graph.extract_key_research_findings_and_methodology_node.logger") as mock_logger:
+            yield mock_logger
+
+    @pytest.fixture()
     def mock_pipeline_state(self) -> PipelineState:
         return {
-            "state": {"text": "Mocked extracted text"}
+            "state": {
+                "text": "Mocked extracted text",
+                "paper_id": "paper_id"
+            }
         }
 
     @pytest.fixture()
     def mock_pipeline_state_with_error(self) -> PipelineState:
         return {
-            "state": {}  # No "text" key to trigger an error
+            "state": {
+                "paper_id": "paper_id"  # Missing "text" key to trigger an error
+            }
         }
 
     @pytest.fixture()
@@ -33,12 +43,16 @@ class TestExtractKeyResearchFindingsAndMethodologyNode:
         self,
         mock_pipeline_state: PipelineState,
         mock_extract_key_research_findings_and_methodology_task: MagicMock,
+        mock_logger: MagicMock,
         extract_key_research_findings_and_methodology: ExtractKeyResearchFindingsAndMethodology
     ) -> None:
         """Test ExtractKeyResearchFindingsAndMethodology node to verify the state output."""
         result = extract_key_research_findings_and_methodology(mock_pipeline_state)
 
         mock_extract_key_research_findings_and_methodology_task.assert_called_once_with("Mocked extracted text")
+        mock_logger.info.assert_called_once_with(
+            "Extracting key research findings and methodology from paper ID paper_id"
+        )
         assert result == {
             "state": {
                 "research": {"key_findings": ["Finding 1", "Finding 2"], "methodology": "Experimental"}
@@ -48,8 +62,12 @@ class TestExtractKeyResearchFindingsAndMethodologyNode:
     def test_extract_key_research_findings_and_methodology_raises_graph_error(
         self,
         mock_pipeline_state_with_error: PipelineState,
+        mock_logger: MagicMock,
         extract_key_research_findings_and_methodology: ExtractKeyResearchFindingsAndMethodology
     ) -> None:
         """Test ExtractKeyResearchFindingsAndMethodology raises GraphError on exception."""
         with pytest.raises(GraphError):
             extract_key_research_findings_and_methodology(mock_pipeline_state_with_error)
+        mock_logger.error.assert_called_once_with(
+            "Failed to extract key research findings and methodology from paper ID paper_id: 'text'"
+        )
