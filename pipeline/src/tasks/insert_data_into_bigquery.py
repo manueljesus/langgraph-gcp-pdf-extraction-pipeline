9ae86ab7c17274a48921efdda5148281956adf38
Manuel Jesús Escamilla <manueljesus@mjescamilla.com>
2024-12-03 20:46:34 +0100

    feat(pipeline): Add logging into insert_data_into_bigquery task

--- pipeline/src/tasks/insert_data_into_bigquery.py
+++ pipeline/src/tasks/insert_data_into_bigquery.py
@@ -4,6 +4,10 @@ from google.cloud.bigquery import Client
 from src.config import Settings
 from src.utils.hash import generate_unique_hash
 from src.tasks import BigQueryError
+from src.logger import get_logger
+
+logger = get_logger(__name__)
+
 
 def insert_data_into_bigquery(
     paper_id: str,
@@ -19,10 +23,13 @@ def insert_data_into_bigquery(
     dataset_id: str = Settings().bigquery_dataset_id
     client = Client()
 
+    logger.info(f"Inserting data into BigQuery tables for paper ID: {paper_id}")
     _insert_research_papers(client, dataset_id, paper_id, data)
     _insert_authors(client, dataset_id, paper_id, data)
     _insert_keywords(client, dataset_id, paper_id, data)
     _insert_key_research_findings(client, dataset_id, paper_id, data)
+    logger.info(f"Data insertion complete for paper ID: {paper_id}")
+
 
 def _insert_research_papers(
     client: Client,
@@ -48,8 +55,10 @@ def _insert_research_papers(
         "publication_date": data["publication_date"]
     }]
     try:
+        logger.info(f"Inserting research paper data into BigQuery table for paper ID: {paper_id}")
         client.insert_rows_json(f"{client.project}.{dataset_id}.research_papers", research_paper)
     except Exception as e:
+        logger.error(f"Failed to insert research paper data: {e}")
         raise BigQueryError(f"Failed to insert research paper data: {e}")
 
 def _insert_authors(
@@ -75,8 +84,10 @@ def _insert_authors(
         raise BigQueryError(f"Failed to insert authors data: {e}")
 
     try:
+        logger.info(f"Inserting authors data into BigQuery table for paper ID: {paper_id}")
         client.insert_rows_json(f"{client.project}.{dataset_id}.authors_x_research_papers", authors, ignore_unknown_values=True, skip_invalid_rows=True)
     except Exception as e:
+        logger.error(f"Failed to insert authors_x_research_papers data: {e}")
         raise BigQueryError(f"Failed to insert authors_x_research_papers data: {e}")
 
 def _insert_keywords(
@@ -97,13 +108,17 @@ def _insert_keywords(
     keywords = list(map(lambda keyword: {'keyword_id': generate_unique_hash(keyword), 'keyword': keyword, 'paper_id': paper_id}, data['keywords']))
 
     try:
+        logger.info(f"Inserting keywords data into BigQuery table for paper ID: {paper_id}")
         client.insert_rows_json(f"{client.project}.{dataset_id}.keywords", keywords, ignore_unknown_values=True, skip_invalid_rows=True)
     except Exception as e:
+        logger.error(f"Failed to insert keywords data: {e}")
         raise BigQueryError(f"Failed to insert keywords data: {e}")
 
     try:
+        logger.info(f"Inserting keywords_x_research_papers data into BigQuery table for paper ID: {paper_id}")
         client.insert_rows_json(f"{client.project}.{dataset_id}.keywords_x_research_papers", keywords, ignore_unknown_values=True, skip_invalid_rows=True)
     except Exception as e:
+        logger.error(f"Failed to insert keywords_x_research_papers data: {e}")
         raise BigQueryError(f"Failed to insert keywords_x_research_papers data: {e}")
 
 def _insert_key_research_findings(
@@ -124,6 +139,8 @@ def _insert_key_research_findings(
     findings = list(map(lambda finding: {'paper_id': paper_id, 'finding': finding}, data['key_research_findings']))
 
     try:
+        logger.info(f"Inserting key research findings data into BigQuery table for paper ID: {paper_id}")
         client.insert_rows_json(f"{client.project}.{dataset_id}.key_research_findings", findings)
     except Exception as e:
+        logger.error(f"Failed to insert key research findings data: {e}")
         raise BigQueryError(f"Failed to insert key research findings data: {e}")
